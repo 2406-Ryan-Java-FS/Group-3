522c817e6112bd9b090ad05ddeee40af32dbd9bd
mrCouture <renestephen427@revature.net>
2024-07-10 12:22:59 -0400

    Added a couple spring profiles because they can be very useful for configuring on different environments.

--- runBackend.sh
+++ runBackend.sh
@@ -1,4 +1,4 @@
 
 
 echo "Shell script: Running the jar"
-java -jar project1-backend/target/project1-0.0.1-SNAPSHOT.jar
+java -jar project1-backend/target/project1-0.0.1-SNAPSHOT.jar --spring.profiles.active=myLocal
