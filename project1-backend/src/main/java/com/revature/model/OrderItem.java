f5dd4d61a108725e9a05f78d29d19499906d6c85
ejlambert <eric.j.lambert@gmail.com>
2024-07-10 17:55:28 -0400

    OrderItem model set up

--- project1-backend/src/main/java/com/revature/model/OrderItem.java
+++ project1-backend/src/main/java/com/revature/model/OrderItem.java
@@ -1,7 +1,85 @@
 package com.revature.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
 
 @Entity
+@Table(name = "order_item")
 public class OrderItem {
+
+    @Id
+    private Long id;
+
+    private int order_id;
+    private int product_id;
+    private int quantity;
+
+    public OrderItem(){
+
+    }
+
+    public OrderItem(Long id, int order_id, int product_id, int quantity){
+        this.id = id;
+        this.order_id = order_id;
+        this.product_id = product_id;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", order_id=" + order_id +
+                ", product_id=" + product_id +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return order_id == orderItem.order_id && product_id == orderItem.product_id && quantity == orderItem.quantity && Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order_id, product_id, quantity);
+    }
 }
