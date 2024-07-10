8b14c68698bbf11e79590663c783cace0c87ac04
ejlambert <eric.j.lambert@gmail.com>
2024-07-10 17:50:09 -0400

    category model set up

--- project1-backend/src/main/java/com/revature/model/Category.java
+++ project1-backend/src/main/java/com/revature/model/Category.java
@@ -1,4 +1,63 @@
 package com.revature.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
