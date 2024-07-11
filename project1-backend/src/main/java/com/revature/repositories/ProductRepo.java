3e2b49cf81c3de38637d61268b7e10c4eaee897d
ejlambert <eric.j.lambert@gmail.com>
2024-07-11 17:16:49 -0400

    created empty repos

---
+++ project1-backend/src/main/java/com/revature/repositories/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.revature.repositories;
+
+import com.revature.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
