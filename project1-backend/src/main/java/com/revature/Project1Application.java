ade25e482920782dba0adf1f79082fc3db47f912
ejlambert <eric.j.lambert@gmail.com>
2024-07-12 17:39:02 -0400

    deleted actuator dependancy allowed merged project to work with get products and categories

--- project1-backend/src/main/java/com/revature/Project1Application.java
+++ project1-backend/src/main/java/com/revature/Project1Application.java
@@ -1,6 +1,6 @@
 package com.revature;
 
-//import javax.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan("com.revature.model")
 @EnableJpaRepositories("com.revature.repositories")public class Project1Application {
 
