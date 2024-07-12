824c022cbcbf9c527a1eac918fdf51b8d2b9de9a
ejlambert <eric.j.lambert@gmail.com>
2024-07-12 15:34:08 -0400

    merge attempt, 1 security bug, new landing page appeared at local 8080

--- project1-backend/src/main/java/com/revature/model/Order.java
+++ project1-backend/src/main/java/com/revature/model/Order.java
@@ -1,4 +1,18 @@
 package com.revature.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "orders", schema = "project1")
+@Data
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int userId;
+    private String status;
+
 }
