ce8bfc9d3ce965a9238bfe8778aa83fd2e58d460
mrCoutureLocal <>
2024-07-16 12:19:18 -0400

    Added registration page

--- project1-frontend/src/components/login.js
+++ project1-frontend/src/components/login.js
@@ -5,7 +5,7 @@ return(<>
     <h1>Welcome</h1>
         <table>
             <tr><td>Username:</td><td><input type="textbox"/></td></tr>
-            <tr><td>Password:</td><td><input type="textbox"/></td></tr>
+            <tr><td>Password:</td><td><input type="password"/></td></tr>
             <tr><td></td><td><button>Login</button></td></tr>
         </table>
     </center>
--- project1-frontend/src/components/register.js
+++ project1-frontend/src/components/register.js
@@ -1,7 +1,14 @@
-export default function Register() {
-    console.log("Register");
-
-    return(<>
-        <h1>Register</h1>
-        </>)
+export default function Register() 
+{
+return(<>
+    <center>
+        <h1>Welcome Newcomer</h1>
+        <table>
+            <tr><td>Username:</td><td><input type="textbox"/></td></tr>
+            <tr><td>Password:</td><td><input type="password" /></td></tr>
+            <tr><td>Password Confirm:</td><td><input type="password" /></td></tr>
+            <tr><td></td><td><button>Register New Account</button></td></tr>
+        </table>
+    </center>
+    </>)
 }
