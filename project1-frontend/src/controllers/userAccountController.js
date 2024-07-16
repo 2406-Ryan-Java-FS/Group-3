5b5273fbee0277d37c37824a3367aa01d03ab9ea
mrCoutureLocal <>
2024-07-16 11:55:41 -0400

    Added BrowserRouter navbar and routes and some css

---
+++ project1-frontend/src/controllers/userAccountController.js
@@ -0,0 +1,17 @@
+
+/*
+    Attemping to use controllers on the frontend.
+    These can manage state/data.
+    These can hold functions which can be called from React OR qunit for testing.
+
+    Attempting to make the app testable.
+    React side will go through menus/pages/modals and 
+    make things look pretty,  but I would like it to be 
+    as dumb as possible so we aren't tangled in state management hell.
+*/
+export default class userAccountController
+{
+    loggedInUser=null
+
+    
+}
--- project1-frontend/src/index.js
+++ project1-frontend/src/index.js
@@ -7,16 +7,31 @@ import Navbar from './components/navbar';
 import Product from './components/product';
 import ProductList from './components/productlist';
 import Register from './components/register';
+import {Routes,Route, BrowserRouter,Link} from 'react-router-dom';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Cart />
-    <Checkout />
-    <Login />
-    <Navbar />
-    <Product />
-    <ProductList />
-    <Register />    
+    <BrowserRouter>
+    {/* Links and Routes must be within BrowserRouter to avoid Errors*/}
+    <nav>
+    <Link to="/">Login</Link> ~ 
+    <Link to="/register"> Register</Link> ~ 
+    <Link to="/cart"> Cart</Link> ~ 
+    <Link to="/checkout"> Checkout</Link> ~ 
+    <Link to="/product"> Product</Link> ~ 
+    <Link to="/product-list"> Product List</Link>
+    </nav>
+    <Routes>
+      <Route path='' element={<Login/>}/>
+      <Route path='register' element={<Register/>}/>
+      <Route path='cart' element={<Cart/>}/>
+      <Route path='checkout' element={<Checkout/>}/>
+      <Route path='product' element={<Product/>}/>
+      <Route path='product-list' element={<ProductList/>}/>
+    </Routes>
+    </BrowserRouter>
+  
   </React.StrictMode>
 );
