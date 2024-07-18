42d9f81f95fe560e2823256d5a9d1fd36908e0bc
ejlambert <eric.j.lambert@gmail.com>
2024-07-18 17:42:04 -0400

    Address now works for CRUD operations over endpoints

--- project1-backend/src/main/java/com/revature/controllers/AddressController.java
+++ project1-backend/src/main/java/com/revature/controllers/AddressController.java
@@ -3,9 +3,10 @@ package com.revature.controllers;
 import com.revature.model.Address;
 import com.revature.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +24,23 @@ public class AddressController {
     @GetMapping
     public List<Address> getAllAddresses(){
         return as.getAllAddresses();
+    }
+
+    @GetMapping("/{id}")
+    public List<Address> getAddressesByUserId(@PathVariable int id){
+        return as.getAddressByUserId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> addAddress(@RequestBody Address a){
+        a = as.addAddress(a);
+
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteProduct(@PathVariable int id) {
+        boolean wasDeleted = as.deleteAddress(id);
+        return new ResponseEntity<>(wasDeleted? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
     }
 }
