eef5c4f42d69aa3bbb86dde639ab246c34553fd6
mrCoutureLocal <>
2024-07-19 12:58:18 -0400

    Changed cart so it fetches when a Route changes to it.

--- project1-frontend/src/components/cart.js
+++ project1-frontend/src/components/cart.js
@@ -1,11 +1,13 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
+let products;
 
 export default function Cart()
 {
-  const { products, setProducts } = useState();
+  //const { products, setProducts } = useState();
 //   const dispatch = useContext(CartDispatchContext);
+  const location=useLocation();
 
   const handleRemove = (productId) => {
   };
@@ -15,24 +17,58 @@ export default function Cart()
   };
 
   useEffect(()=>{
-    async function getAllProducts() {
+      async function yayReact(){
         const url = "http://localhost:8080/products";
         const httpResponse = await fetch(url);
-        const productList = await httpResponse.json();
-        setProducts(productList);
-    }
-    // getAllProducts();
-  }, [])
+        products = await httpResponse.json();
+        //setProducts(productList);
+      }
+      yayReact()
+  },[location])
 
+// return (<>
+// <h1>Cart</h1>
+// <table>
+//   <th><td>Price</td><td>Name</td>
+  
+//   </th>
+//   {products && products.map((product) => {
+//     return (
+//       <li className="cart-item" key={product.name}>
+//         {/* <img className="product-image" src={product.image} alt='product-image'/> */}
+//         <div className="product-info">
+//           <p className="product-name">{product.name}</p>
+//           <p className="product-price">{product.price}</p>
+//         </div>
+//         <div className="product-quantity">
+//           <div className="quantity-control" onClick={() => handleItemQuantity(product.id, product.quantity + 1)}>+</div>
+//             <p>{product.quantity}</p>
+//           <div className="quantity-control" onClick={() => handleItemQuantity(product.id, product.quantity - 1)}>-</div>
+//         </div>
+//         <div className="product-total">
+//           <p className="amount">{product.quantity * product.price}</p>
+//         </div>
+//         <button
+//           className="product-remove"
+//           onClick={() => handleRemove(product.id)}
+//         >
+//           ×
+//         </button>
+//       </li>
+//     );
+//   })}
+// </table>
+// </>);
 
 
   return (
     <div>
+      <h1>Cart</h1>
       <ul className="cart-items">
         {products && products.map((product) => {
           return (
             <li className="cart-item" key={product.name}>
-              <img className="product-image" src={product.image} />
+              {/* <img className="product-image" src={product.image} alt='product-image'/> */}
               <div className="product-info">
                 <p className="product-name">{product.name}</p>
                 <p className="product-price">{product.price}</p>
