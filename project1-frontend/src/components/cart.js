19650307ebac9ddec1f167f809e721e0dd7bbd39
Jeet Patel <pateljeet33@gmail.com>
2024-07-19 16:54:29 -0500

    Adding some work on Cart component

--- project1-frontend/src/components/cart.js
+++ project1-frontend/src/components/cart.js
@@ -1,7 +1,22 @@
+import { useContext } from "react"
+import { AppContext } from "../AppContext"
+
 export default function Cart() {
-    console.log("Cart");
+    // console.log("Cart");
+    const {cart} = useContext(AppContext);
 
     return (<>
-    <h1>Cart</h1>
+        <h1>Cart</h1>
+        {cart.length === 0 ? (
+            <p>No items in cart...</p>
+        ): (
+            <ul>
+                {cart.map((item,index) => (
+                    <li key={index}>
+                        {item.name} - ${item.price}
+                    </li>
+                ))}
+            </ul>
+        )}
     </>)
 }
